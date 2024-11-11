27cff781aa88e14bd1f75021316ce8578b02d845
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2024-11-11 12:07:19 +0000

    feat(styles/colors.ts): added light and dark themes.

---
+++ styles/colors.ts
@@ -0,0 +1,17 @@
+const lightTheme = {
+  background: "#FFFFFF",
+  headerBackground: "#D32F2F",
+  headerText: "#FFFFFF",
+  textColor: "#000000",
+  buttonBackground: "#D32F2F",
+  buttonTextColor: "#FFFFFF",
+};
+
+const darkTheme = {
+  background: "#424242",
+  headerBackground: "#333333",
+  headerText: "#5A6A73",
+  textColor: "#FFFFFF",
+  buttonBackground: "#333333",
+  buttonTextColor: "#5A6A73",
+};
